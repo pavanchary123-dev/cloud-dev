78ef851708b981bf500b00862658aef2549bbc62
pavan <sankojipavanchary123@gmail.com>
2025-12-25 10:24:10 +0530

    new change

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ number = int(input("Enter a number: "))
 if number % 2 == 0:
     print("Even number")
 else:
-    print("Odd number")
+    print("Odd numbers")
     print("new change-1")
     print("2nd changes")
